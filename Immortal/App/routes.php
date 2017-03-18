a4fa4a5073a51830876e33b06807948378340b58
Javed Ahmadzai <theahmadzai@hotmail.com>
2017-03-18 16:57:48 +0500

    Cleaned

--- Immortal/App/routes.php
+++ Immortal/App/routes.php
@@ -4,16 +4,7 @@ use Immortal\View;
 
 Route::set('/', 'Home@index');
 
-Route::set('users', 'Users@index');
-
-Route::set('user', 'User@index');
-
-Route::set('user/:name', 'User@name');
-
-Route::set('comments/:id/:?name', 'Comments@index');
-
-Route::set('test/:id/:?name', function ($id = 13, $name = 'javed')
-{
+Route::set('test/:id/:?name', function ($id = 13, $name = 'javed') {
     $data = ['id' => $id, 'name' => $name];
 
     return View::make('index.twig', $data);
